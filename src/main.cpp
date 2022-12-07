e76c144325002f7e358dc176403edbe5ae3d3716
Replit user <>
2022-12-07 17:49:21 +0000

    Letuchka 4

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+struct MobilePhone {
+  double weight;
+  bool is_broken;
+  double price;
+};
+
+void printPhoneStruct(MobilePhone* phone_ptr){
+  std::cout << "Вес мобилы: " << phone_ptr->weight << std::endl;
+  std::cout << "Мобила сломана: " << phone_ptr->is_broken << std::endl;
+  std::cout << "Сколько стоит мобила: " << phone_ptr->price << std::endl;
+}
+
+void printXML(MobilePhone* phones, int n){
+  std::cout << "<MobilePhones>" << std::endl;
+  for (int i = 0; i < n; i++) {
+    std::cout << "\t" << "<MobilePhones id=" << '"' << i << '"' << " weight=" << '"' << phones[i].weight
+              << '"' << " is_broken=" << '"' << std::boolalpha << phones[i].is_broken << '"'
+              << " price=" << '"' << phones[i].price << "\"/>" << std::endl;
+  }
+  std::cout << "</MobilePhones>" << std::endl;
+}
+
+int main(){
+  unsigned int number;
+  std::cout << "Введите количество мобил: ";
+  std::cin >> number;
+  MobilePhone phones[number];
+
+  for (int i=0; i<number; i++){
+    std::cout << "Введите данные мобилы №" << i+1 << std::endl;
+    std::cout << "Введите вес мобилы: ";
+    std::cin >> phones[i].weight;
+    std::cout << "Введите состояние мобилы: ";
+    std::cin >> phones[i].is_broken;
+    std::cout << "Введите цену: ";
+    std::cin >> phones[i].price;
+  }
+
+   printXML(phones, number);
+
+  // for (int i=0; i<number; i++){
+  //   printPhoneStruct(&phones[i]);
+  // }
+
+  return 0;
+}
